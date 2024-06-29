a5cc4746a7ea87746954d6411983849d07d7359b
jonathanyangg <jonathan.yang.x@gmail.com>
2024-06-29 13:53:05 -0400

    static file string syntax

--- catalogproject/settings.py
+++ catalogproject/settings.py
@@ -265,7 +265,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

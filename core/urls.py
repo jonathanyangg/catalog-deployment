547480eb767d36d36c59babdf016afe37d361b83
jonathanyangg <jonathan.yang.x@gmail.com>
2024-06-30 11:11:34 -0400

    comment

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import test_env_vars
+# from .views import test_env_vars
 
 urlpatterns = [
     path('', views.index, name = 'index'),

f6aba7e0655ab123c8fffe85abc125765d0cc14f
jonathanyangg <jonathan.yang.x@gmail.com>
2024-07-02 11:04:11 -0400

    debug false

--- catalogproject/settings.py
+++ catalogproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lawrencevilleaicoursecatalog.azurewebsites.net', '127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://lawrencevilleaicoursecatalog.azurewebsites.net', ]
